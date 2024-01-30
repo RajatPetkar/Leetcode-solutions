840c7dddddfee63676d059bdb2e0ada58fcc04b7
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-01-30 21:34:51 +0530

    Time: 49 ms (91.67%), Space: 49.2 MB (16.95%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length;
+    let first = cost[0];
+	let second = cost[1];
+	if (n<=2) return Math.min(first, second);
+	for (let i=2; i<n; i++) {
+		let curr = cost[i] + Math.min(first, second);
+		first = second;
+		second = curr;
+	}
+	return Math.min(first, second);
+};
