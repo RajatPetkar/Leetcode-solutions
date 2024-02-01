60bb0846eb1fbc0c0c21b2c602b4a219786caba5
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-02-01 11:18:55 +0530

    Time: 206 ms (71.99%), Space: 68.2 MB (68.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,6 +3,47 @@
  * @return {number[]}
  */
 
-var sortArray = function(nums) {
-     return nums.sort((a,b)=>a-b)
+var sortArray = function(arr) {
+    let s = 0;
+    let e = arr.length -1
+    mergeSort(arr,s,e);
+    return arr
 };
+function mergeSort(arr,s,e){
+    var mid=Math.floor((s+e)/2)
+    if(s>=e)return;
+    mergeSort(arr,s,mid)
+    mergeSort(arr,mid+1,e)
+    merge(arr,s,e)
+}
+function merge(arr, s, e) {
+    var mid = Math.floor((e - s) / 2) + s;
+    let first = []
+    let second = []
+
+    let k = s;
+    for (let i = 0; i <= mid - s; i++) {
+        first[i] = arr[k++]
+    }
+    k = mid + 1;
+    for (let i = 0; i < e - mid; i++) {
+        second[i] = arr[k++]
+    }
+
+    let index1 = 0
+    let index2 = 0
+    k = s
+    while (index1 < first.length && index2 < second.length) {
+        if (first[index1] < second[index2]) {
+            arr[k++] = first[index1++]
+        } else {
+            arr[k++] = second[index2++]
+        }
+    }
+    while (index1 < first.length) {
+        arr[k++] = first[index1++]
+    }
+    while (index2 < second.length) {
+        arr[k++] = second[index2++]
+    }
+}
