a38c0239fe18c462e92e3f0b998dcef2b6379d98
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-06-17 09:20:29 +0530

    Time: 4 ms (46.70%), Space: 7 MB (61.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+    int a = 0;
+    int b = static_cast<int>(sqrt(c));
+
+    while (a <= b) {
+        long long sum = static_cast<long long>(a) * a + static_cast<long long>(b) * b;
+        if (sum == c) {
+            return true;
+        } else if (sum < c) {
+            a++;
+        } else {
+            b--;
+        }
+    }
+    
+    return false;
+}
+
+};
