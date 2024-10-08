23d72bf8765821fb5a6c56449b60ecb302b59159
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-10-08 14:12:25 +0530

    Time: 101 ms (70.15%), Space: 32.5 MB (98.20%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+       int size = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            if (ch == '[') 
+                size++;
+            else if (size > 0) 
+                size--;
+        }
+        return (size + 1) / 2;  
+    }
+};
