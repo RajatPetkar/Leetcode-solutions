9b427d454be9f145535d737beb57b49974e44b53
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-11-02 21:44:57 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (31.30%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    vector<string> splitter(string s) {
-        string s1 = "";
-        vector<string> v;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == ' ') { 
-                if (!s1.empty()) { 
-                    v.push_back(s1);
-                    s1 = "";
-                }
-            } else {
-                s1 += s[i];
-            }
-        }
-        if (!s1.empty()) {
-            v.push_back(s1);
-        }
-        return v;
-    }
+    // solution 1
+//     vector<string> splitter(string s) {
+//         string s1 = "";
+//         vector<string> v;
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s[i] == ' ') { 
+//                 if (!s1.empty()) { 
+//                     v.push_back(s1);
+//                     s1 = "";
+//                 }
+//             } else {
+//                 s1 += s[i];
+//             }
+//         }
+//         if (!s1.empty()) {
+//             v.push_back(s1);
+//         }
+//         return v;
+//     }
 
-    bool isCircular(vector<string> v) {
-        int n = v.size();
-        for (int i = 0; i < n; i++) {
-            int j = (i + 1) % n;
-            if (v[i].back() != v[j].front()) {
-                return false;
-            }
-        }
-        return true;
-    }
+//     bool isCircular(vector<string> v) {
+//         int n = v.size();
+//         for (int i = 0; i < n; i++) {
+//             int j = (i + 1) % n;
+//             if (v[i].back() != v[j].front()) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
 
     // bool isCircularSentence(string sentence) {
     //     vector<string> words = splitter(sentence);
     //     return isCircular(words);
     // }
+    
+    
+    //------------------- Solution 2 -----------------------
     
     bool isCircularSentence(string sentence) {
         int n = sentence.size();
