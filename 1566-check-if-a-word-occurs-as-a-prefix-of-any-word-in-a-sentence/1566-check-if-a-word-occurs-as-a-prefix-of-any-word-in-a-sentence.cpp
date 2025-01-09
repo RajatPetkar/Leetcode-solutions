1899026d3108cf20e19e7490a65ad8d8a737c03c
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-01-09 22:33:05 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.1%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,29 +1,62 @@
 class Solution {
 public:
-    vector<string> splitter(string s){
-        vector<string> arr;
-        string sr;
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=' '){
-                sr+=s[i];
-            }else{
-                arr.push_back(sr);
-                sr = "";
-            }
-        }
-        if (!sr.empty()) {
-            arr.push_back(sr);
-        }
-        return arr;
-    }
+
+    //solution 1 - more Space complexity 
+
+    // vector<string> splitter(string s){
+    //     vector<string> arr;
+    //     string sr;
+    //     for(int i=0;i<s.length();i++){
+    //         if(s[i]!=' '){
+    //             sr+=s[i];
+    //         }else{
+    //             arr.push_back(sr);
+    //             sr = "";
+    //         }
+    //     }
+    //     if (!sr.empty()) {
+    //         arr.push_back(sr);
+    //     }
+    //     return arr;
+    // }
+    // int isPrefixOfWord(string sentence, string searchWord) {
+    //  vector<string> s;
+    //  s = splitter(sentence);  
+    //  for(int i=0;i<s.size();i++){
+    //      if (s[i].substr(0, searchWord.size()) == searchWord) {
+    //         return i+1;
+    //     }
+    //  } 
+    //  return -1;
+    // }
+
+    // solution 2
+
     int isPrefixOfWord(string sentence, string searchWord) {
-     vector<string> s;
-     s = splitter(sentence);  
-     for(int i=0;i<s.size();i++){
-         if (s[i].substr(0, searchWord.size()) == searchWord) {
-            return i+1;
+    int wordIndex = 1;
+    int i = 0, n = sentence.length();
+
+    while (i < n) {
+        int start = i;
+       
+        while (i < n && sentence[i] != ' ') {
+            i++;
         }
-     } 
-     return -1;
+
+       
+        string currentWord = sentence.substr(start, i - start);
+
+       
+        if (currentWord.substr(0, searchWord.size()) == searchWord) {
+            return wordIndex;
+        }
+
+        
+        wordIndex++;
+        i++;
     }
+
+    return -1; // No match found
+}
+
 };
