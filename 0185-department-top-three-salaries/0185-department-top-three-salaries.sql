6d40dcd1f9212f9b23beee06fadf81ccb37c0052
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-01-10 08:10:28 +0530

    Time: 1308 ms (11.95%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,19 @@
+/* Write your T-SQL query statement below */
+--ms sql server
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM
+    Department d
+    JOIN Employee e ON d.id = e.departmentId
+WHERE
+    e.salary IN (
+        SELECT DISTINCT TOP 3 salary
+        FROM Employee e2
+        WHERE e2.departmentId = d.id
+        ORDER BY salary DESC
+    )
+ORDER BY
+    Department, Salary DESC;
+	
