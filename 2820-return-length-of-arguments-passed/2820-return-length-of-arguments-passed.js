27f14b9307573b25a9be1e6cf80885047bf55b8d
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-05-01 00:55:52 +0530

    Time: 39 ms (69.35%), Space: 53.8 MB (32.35%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    // let cnt = 0;
+    // for(let i=0;i<args.length;i++){
+    //     cnt++;
+    // }
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
