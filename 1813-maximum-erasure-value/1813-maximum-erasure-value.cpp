2026b81028da2540cc22aab7142c2929d03a6a23
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-07-22 15:22:13 +0530

    Time: 65 ms (81.38%), Space: 103.3 MB (78.52%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    int maximumUniqueSubarray(vector<int>& nums) {
-       unordered_set<int> set;
-       int maxSum = 0, currSum = 0;
-       int left = 0;
+    // int maximumUniqueSubarray(vector<int>& nums) {
+    //    unordered_set<int> set;
+    //    int maxSum = 0, currSum = 0;
+    //    int left = 0;
 
-        for (int right = 0; right < nums.size(); ++right) {
-            while (set.count(nums[right])) {
-                currSum -= nums[left];
-                set.erase(nums[left]);
-                ++left;
+    //     for (int right = 0; right < nums.size(); ++right) {
+    //         while (set.count(nums[right])) {
+    //             currSum -= nums[left];
+    //             set.erase(nums[left]);
+    //             ++left;
+    //         }
+    //         set.insert(nums[right]);
+    //         currSum += nums[right];
+    //         maxSum = max(maxSum, currSum);
+    //     }
+    //     return maxSum;
+    // }
+
+    int maximumUniqueSubarray(vector<int>& nums) {
+    unordered_map<int, int> lastIndex;
+    int maxSum = 0, currSum = 0;
+    int left = 0;
+    for (int right = 0; right < nums.size(); ++right) {
+        if (lastIndex.count(nums[right]) && lastIndex[nums[right]] >= left) {
+            int prevIdx = lastIndex[nums[right]];
+            for (int i = left; i <= prevIdx; ++i) {
+                currSum -= nums[i];
             }
-            set.insert(nums[right]);
-            currSum += nums[right];
-            maxSum = max(maxSum, currSum);
+            left = prevIdx + 1;
         }
-        return maxSum;
+        currSum += nums[right];
+        lastIndex[nums[right]] = right;
+        maxSum = max(maxSum, currSum);
     }
+    return maxSum;
+}
 };
