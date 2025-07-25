918ebb8a1c554a1fb65ccf0cccc0a4d2ba6918fa
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-07-25 14:47:31 +0530

    Time: 0 ms (100%), Space: 29.3 MB (94.93%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    int maxSum(vector<int>& nums) {
-        int result=INT_MIN,sum=0;
-        set<int,greater<int>> s(nums.begin(), nums.end());
-        for(int num:s)
-        {
-            sum+=num;
-            result=max(result,sum);
-        }
-        return result;
-    }
+   int maxSum(vector<int>& nums) {         
+   int result = INT_MIN, sum = 0;         
+   sort(nums.begin(), nums.end());         
+   for(int i = 0; i < nums.size(); i++)         
+   {             
+       if(i < nums.size() - 1 && nums[i] == nums[i+1]) {
+           continue;
+       }
+       sum += nums[i];             
+       result = max(result, sum);
+       if(nums[i] < 0) {
+           sum = 0;  // Reset sum after negative number
+       }
+   }         
+   return result;     
+}
 };
