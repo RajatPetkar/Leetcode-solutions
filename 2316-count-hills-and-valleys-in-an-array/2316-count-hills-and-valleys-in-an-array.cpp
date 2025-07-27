35e2dd23bbb3bc69ec251ae30cfb6c25f973309a
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-07-27 21:56:56 +0530

    Time: 0 ms (100%), Space: 12.1 MB (77.89%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    int countHillValley(vector<int>& nums) {
-        vector<int> arr;
-        arr.push_back(nums[0]);
+
+    // O(N) BOTH SAPCE AND TIME
+
+    // int countHillValley(vector<int>& nums) {
+    //     vector<int> arr;
+    //     arr.push_back(nums[0]);
+
+    //     for (int i = 1; i < nums.size(); i++) {
+    //         if (nums[i] != nums[i - 1]) arr.push_back(nums[i]);
+    //     }
         
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[i - 1]) arr.push_back(nums[i]);
+    //     int cnt = 0;
+    //     for (int i = 1; i < arr.size() - 1; i++) {
+    //         if ((arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) || 
+    //             (arr[i] < arr[i - 1] && arr[i] < arr[i + 1])) {
+    //             cnt++;
+    //         }
+    //     }
+    //     return cnt;
+    // }
+
+
+    // O(N) TIME && O(1) SPACE
+
+   int countHillValley(vector<int>& nums) {
+    int n = nums.size();
+    int k = 1;
+    for (int i = 1; i < n; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k++] = nums[i];
         }
-        
-        int cnt = 0;
-        for (int i = 1; i < arr.size() - 1; i++) {
-            if ((arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) || 
-                (arr[i] < arr[i - 1] && arr[i] < arr[i + 1])) {
-                cnt++;
-            }
-        }
-        return cnt;
     }
+    
+    int cnt = 0;
+    for (int i = 1; i < k - 1; i++) {
+        if ((nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) || 
+            (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
 };
