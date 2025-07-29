307e21b2430939197e8d5fc92933002baaef3ee5
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-07-29 19:02:36 +0530

    Time: 7 ms (73.1%), Space: 22.9 MB (73.79%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -3,21 +3,22 @@ public:
     long long func(vector<int>& piles, int hourly) {
         long long totalTime = 0;
         for (int i = 0; i < piles.size(); i++) {
-            totalTime += (piles[i] + hourly - 1LL) / hourly;
+            totalTime += (piles[i] + hourly - 1) / hourly;
         }
         return totalTime;
-    }
-
+    } 
+     
+    
     int minEatingSpeed(vector<int>& piles, int h) {
         int left = 1;
         int right = *max_element(piles.begin(), piles.end());
 
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = (left+right) / 2;
             long long reqTime = func(piles, mid);
 
             if (reqTime <= h) {
-                right = mid; 
+                right = mid;
             } else {
                 left = mid + 1;
             }
