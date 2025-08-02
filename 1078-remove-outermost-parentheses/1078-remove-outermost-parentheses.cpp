6badc59bfe7327daf45e1e338932ab1a7efc55c0
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-08-02 14:37:26 +0530

    Time: 1 ms (27.57%), Space: 10.2 MB (10.85%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
+    // string removeOuterParentheses(string s) {
+    //     string result;
+    //     int Count = 0;
+
+    //     for (char c : s) {
+    //         if (c == '(') {
+    //             if (Count > 0) result += c;
+    //             Count++;
+    //         } else {
+    //             Count--;
+    //             if (Count > 0) result += c;
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
     string removeOuterParentheses(string s) {
-        string result;
-        int Count = 0;
-        
-        for (char c : s) {
-            if (c == '(') {
-                if (Count > 0) result += c;
-                Count++;
+        stack<char> stk;
+        string ans;
+        int i = 0;
+        int n = s.size();
+
+        while (i < n) {
+            if (s[i] == '(') {
+                if (!stk.empty()) {
+                    ans += '(';
+                }
+                stk.push('(');
             } else {
-                Count--;
-                if (Count > 0) result += c; 
+                if (!stk.empty()) {
+                    stk.pop();
+                    if (!stk.empty()) {
+                        ans += ')';
+                    }  
+                }
             }
+            i++;
         }
-        
-        return result;
+        return ans;
     }
 };
