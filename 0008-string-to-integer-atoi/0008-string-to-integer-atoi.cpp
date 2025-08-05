9a5c6aaad5ba53e34887a362dcdcad419c5058c4
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-08-05 14:29:19 +0530

    Time: 1 ms (23.78%), Space: 9.2 MB (23.71%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,53 @@
+// class Solution {
+// public:
+//     int myAtoi(string s) {
+//         int ans = 0;  
+//         int sign = 1;
+
+//         for (int i = 0; i < s.size(); i++) {
+            
+//             if (!isdigit(s[i])) {
+//                 if (s[i] == '-') {
+//                     sign = -1;
+//                     continue;  
+//                 } else if (s[i] == '+') {
+//                     continue;   
+//                 } else {
+//                     return ans * sign; 
+//                 }
+//             } 
+//             else {
+//                 ans = ans * 10 + (s[i] - '0');  
+//             }
+//         }
+//         return ans * sign;
+//     }
+// };
+
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long ans = 0;  
+        int sign = 1;
+        int i = 0;
+
+        while (i < s.size() && s[i] == ' ') i++;
+
+        if (i < s.size() && (s[i] == '-' || s[i] == '+')) {
+            if (s[i] == '-') sign = -1;
+            i++;
+        }
+
+        for (; i < s.size(); i++) {
+            if (!isdigit(s[i])) break;
+            if(ans > 214748364 || (ans == 214748364 && s[i]- '0' >7)){
+                if (sign==1) return INT_MAX;
+                else return INT_MIN;
+            }
+            ans = ans * 10 + (s[i] - '0');
+        }
+
+        return ans * sign;
+    }
+};
