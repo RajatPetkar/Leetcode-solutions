1461bf7188dcfdfd41c93286587efbf122b23f18
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-08-06 17:54:35 +0530

    Time: 2684 ms (5%), Space: 15.8 MB (12.31%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();
+
+        bool first_match = (!s.empty() && (s[0] == p[0] || p[0] == '.'));
+
+        if (p.size() >= 2 && p[1] == '*') {
+            return (isMatch(s, p.substr(2)) ||  // '*' counts as 0
+                    (first_match && isMatch(s.substr(1), p)));  // '*' counts as 1 or more
+        } else {
+            return first_match && isMatch(s.substr(1), p.substr(1));
+        }
+    }
+};
