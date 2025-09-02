e49fc0fc48d6324b00af31e56b17c60309e05c53
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-09-02 13:53:02 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.62%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombination(0, nums, ans, ds);
+        return ans;
+    }
+
+    void findCombination(int ind, vector<int>& arr, vector<vector<int>>& ans,
+                         vector<int>& ds) {
+        ans.push_back(ds);
+        for (int i = ind; i < arr.size(); i++) {
+            if (i > ind && arr[i] == arr[i - 1])
+                continue;
+
+            ds.push_back(arr[i]);
+            findCombination(i + 1, arr,ans, ds);
+            ds.pop_back();
+        }
+    }
+};
