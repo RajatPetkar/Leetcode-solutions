1fba1d4245b0d5f88589808d56371d7d89640ab4
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2025-09-23 21:10:23 +0530

    Time: 0 ms (100%), Space: 12.9 MB (37.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-                            int count  = 0;
-        vector<int> arr;
+    // vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    //     int count = 0;
+    //     vector<int> arr;
 
-        for(int i = 0;i<nums1.size();i++){
-            for(int j = 0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    count=1;
-                }
-                if(count==1){
-                    if(nums1[i]<nums2[j]){
-                        arr.push_back(nums2[j]);
-                        count=0;
-                    }
-                    else if(j==nums2.size()-1){
-                        arr.push_back(-1);
-                        count=0;
-                    }
-                }
+    //     for (int i = 0; i < nums1.size(); i++) {
+    //         for (int j = 0; j < nums2.size(); j++) {
+    //             if (nums1[i] == nums2[j]) {
+    //                 count = 1;
+    //             }
+    //             if (count == 1) {
+    //                 if (nums1[i] < nums2[j]) {
+    //                     arr.push_back(nums2[j]);
+    //                     count = 0;
+    //                 } else if (j == nums2.size() - 1) {
+    //                     arr.push_back(-1);
+    //                     count = 0;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return arr;
+    // }
+
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nextGreater;
+        stack<int> st;
+
+        for (int num : nums2) {
+            while (!st.empty() && num > st.top()) {
+                nextGreater[st.top()] = num;
+                st.pop();
             }
+            st.push(num);
         }
-        return arr;
+        while (!st.empty()) {
+            nextGreater[st.top()] = -1;
+            st.pop();
+        }
+
+        vector<int> ans;
+        for (int num : nums1) {
+            ans.push_back(nextGreater[num]);
+        }
+        return ans;
     }
 };
