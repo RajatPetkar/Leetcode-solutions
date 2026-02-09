92f35003974067a0609d5a5ad45a249f804895e4
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2026-02-09 14:29:35 +0530

    Time: 0 ms (100%), Space: 38.2 MB (95.22%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,39 +1,55 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        vector<int> mp(26, 0);
-        for (char ch : tasks) {
-            mp[ch - 'A']++;
-        }
+//     int leastInterval(vector<char>& tasks, int n) {
+//         vector<int> mp(26, 0);
+//         for (char ch : tasks) {
+//             mp[ch - 'A']++;
+//         }
 
-        priority_queue<int> pq;
-        for (int i = 0; i < 26; i++) {
-            if (mp[i] > 0) pq.push(mp[i]);
-        }
+//         priority_queue<int> pq;
+//         for (int i = 0; i < 26; i++) {
+//             if (mp[i] > 0) pq.push(mp[i]);
+//         }
 
-        int time = 0;
+//         int time = 0;
 
-        while (!pq.empty()) {
-            vector<int> temp;
-            int cnt = 0;
+//         while (!pq.empty()) {
+//             vector<int> temp;
+//             int cnt = 0;
 
-            for (int i = 0; i <= n; i++) {
-                if (!pq.empty()) {
-                    int freq = pq.top();
-                    pq.pop();
-                    if (--freq > 0) temp.push_back(freq);
-                    cnt++;
-                }
-            }
+//             for (int i = 0; i <= n; i++) {
+//                 if (!pq.empty()) {
+//                     int freq = pq.top();
+//                     pq.pop();
+//                     if (--freq > 0) temp.push_back(freq);
+//                     cnt++;
+//                 }
+//             }
 
-            for (int f : temp) pq.push(f);
+//             for (int f : temp) pq.push(f);
 
-            if (pq.empty())
-                time += cnt;    
-            else
-                time += n + 1;  
-        }
+//             if (pq.empty())
+//                 time += cnt;    
+//             else
+//                 time += n + 1;  
+//         }
 
-        return time;
-    }
+//         return time;
+//     }
+// };
+
+
+int leastInterval(vector<char>& tasks, int n) {
+    vector<int> freq(26, 0);
+    for(char c : tasks) freq[c - 'A']++;
+
+    sort(freq.begin(), freq.end());
+    int maxFreq = freq[25] - 1;
+    int idleSlots = maxFreq * n;
+
+    for(int i = 24; i >= 0 && freq[i] > 0; i--)
+        idleSlots -= min(freq[i], maxFreq);
+
+    return idleSlots > 0 ? idleSlots + tasks.size() : tasks.size();
+}
 };
