198fac315e7dc228af9ac5f2b8876023536ee3ff
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2026-07-13 09:51:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (76.91%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,20 +1,43 @@
+// class Solution {
+// public:
+//     vector<int> ans;
+
+//     void generate(int num, int lastDigit, int low, int high) {
+//         if (num > high || lastDigit > 9)
+//             return;
+//         if (num >= low)
+//             ans.push_back(num);
+//         if(lastDigit ==9 ) return;
+//         generate(num * 10 + (lastDigit + 1), lastDigit + 1, low, high);
+//     }
+//     vector<int> sequentialDigits(int low, int high) {
+//         for (int i = 1; i <= 9; i++) {
+//             generate(i, i, low, high);
+//         }
+//         sort(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    vector<int> ans;
-
-    void generate(int num, int lastDigit, int low, int high) {
-        if (num > high || lastDigit > 9)
-            return;
-        if (num >= low)
-            ans.push_back(num);
-        if(lastDigit ==9 ) return;
-        generate(num * 10 + (lastDigit + 1), lastDigit + 1, low, high);
-    }
     vector<int> sequentialDigits(int low, int high) {
-        for (int i = 1; i <= 9; i++) {
-            generate(i, i, low, high);
+        vector<int> ans;
+
+        string s = "123456789";
+        string l = to_string(low);
+        string h = to_string(high);
+
+        for(int i = l.size(); i <= h.size(); i++) {
+            for(int j = 0; j <= 9 - i; j++) {
+                string n = s.substr(j, i);
+                int num = stoi(n);
+                if(num >= low && num <= high)
+                    ans.push_back(num);
+            }
         }
-        sort(ans.begin(), ans.end());
+
         return ans;
     }
 };
