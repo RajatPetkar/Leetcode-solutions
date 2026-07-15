21037cb10241bc6bb3b66ed1f9efb8a8b07eddf7
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2026-07-15 13:39:07 +0530

    Time: 6 ms (11.8%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int so=0,se=0;
+        for(int i=1;i<=n*2;i++)
+        {
+            if(i%2==0) se+=i;
+            else so+=i;
+        }
+        int y=0;
+        for(int i=1;i<=n*2;i++)
+        {
+            if(so%i==0 && se%i==0) y=max(y,i);
+        }
+        return y;
     }
 };
